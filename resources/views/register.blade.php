8d6e3e42130d7f84fa1ad1369ff77e94f1746529
githoanganh <hoanganh98.pham@gmail.com>
2020-05-18 16:02:57 +0700

    first commit

---
+++ resources/views/register.blade.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/css1.css">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet"/>
+    <title>Register</title>
+</head>
+<body>
+    <div class="container">
+        <div class="wrapper">
+            <div class="header">
+                <h1>MicroOffice</h1>
+            </div>
+            <div class="body">
+                <form method="">
+                    <p> Registration form </p>
+                    <div class="lable-1">
+                            <input class=" lable-1-1" type="text" class="text" placeholder=" &#xf007;   First Name" >
+                            <input type="text" class="text"  placeholder=" &#xf007;  Last Name" >
+                    </div>
+                    <div class="lable">
+                            <input type="text" class="text"  placeholder=" &#xf0e0;  Email address" >
+                    </div>
+                    <div class="lable">
+                        <input type="text" class="text"  placeholder=" &#xf007;  Nickname" >
+                    </div>
+                    <div class="lable">
+                        <input type="text" class="text"  placeholder=" &#xf023;  Create a password" >
+                    </div>
+                    <div class="lable">
+                        <input type="text" class="text"  placeholder=" &#xf023;  Retype your password" >
+                    </div>
+                    <div class="boxcheck">
+                        <label class="checkbox">I agree to the <a href="">teams of use</a>
+                            <input type="checkbox" checked="check" >
+                            <span class="checkmark"></span>
+                        </label>
+                        <input class="sub" type="submit" value="I ACCEPT - CREATE ACCOUNT" >
+                    </div>
+
+
+                </form>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
