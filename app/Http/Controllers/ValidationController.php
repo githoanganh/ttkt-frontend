0a35b67be7b87fc665e2918f1bbb0758a0deac07
githoanganh <hoanganh98.pham@gmail.com>
2020-05-24 20:18:12 +0700

    Update scss

---
+++ app/Http/Controllers/ValidationController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ValidationController extends Controller
+{
+    public function login(){
+        return view('login');
+    }
+    public function register(){
+        return view('register');
+    }
+}
---
+++ resources/views/login.blade.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/login.css">
+    <title>Login</title>
+</head>
+<body>
+<div class="contrainer">
+    <div class="wrapper" >
+        <div class="login">
+            <div class="header">
+                <img src="img/logo.png" alt="">
+                <h2>Simulor</h2>
+            </div>
+            <p>Enter your email address and password to access admin panel</p>
+            <form action="">
+                <div class="log">
+                    <label class="label" >Email addrerss</label>
+                    <br/>
+                    <input class="box" type="email" placeholder="  Enter your email">
+                </div>
+                <div class="log">
+                    <label class="label" >Password</label>
+                    <br/>
+                    <input class="box" type="password" placeholder="  Enter your password">
+                </div>
+                <div class="boxcheck">
+                    <label class="checkbox">Remember me
+                        <input type="checkbox" checked="check" >
+                        <span class="checkmark"></span>
+                    </label>
+                </div>
+                <div class="log">
+                    <button type="submit">Login</button>
+                </div>
+            </form>
+        </div>
+
+    </div>
+    <div class="footer">
+        <a class="light" href="">Forgot your password?</a>
+        <br>
+        <a class="light" href="">Don't have an account?</a>
+        <a class="bold" href="{{url('register')}}">Sign Up</a>
+    </div>
+</div>
+</body>
+</html>
